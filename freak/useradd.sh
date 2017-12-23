0288190bd65bda92b235917cc1b2544f1c9514f3
akumasih112 <30937741+akumasih112@users.noreply.github.com>
2017-12-23 12:28:52 +0700

    Update useradd.sh

--- freak/useradd.sh
+++ freak/useradd.sh
@@ -20,18 +20,22 @@ useradd -e `date -d "$fnsaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "Data Login"
-echo -e "=========-account-=========="
+echo -e "============================"
+echo -e "Informasi Account SSH"
+echo -e "============================"
 echo -e "Host: $IP" 
 echo -e "Username: $Login"
 echo -e "Password: $Pass"
-echo -e "Dropbear Port: 443, 110, 109"
-echo -e "OpenSSH Port: 22, 143, 80"
-echo -e "Squid: 80, 8080, 3128"
-echo -e "OVPN: http://$IP:81/client.ovpn"
+echo -e "SSL/TLS Port : 443, 80"
+echo -e "Dropbear Port: 22, 444, 3128"
+echo -e "OpenSSH Port: 143"
+echo -e "Squid: 8080"
+echo -e "OpenVPN (TCP 1194) : http://$IP:81/client.ovpn"
 echo -e "-----------------------------"
 echo -e "Aktif Sampai: $exp"
 echo -e "==========================="
+echo -e "Script Edited By Roziq Yusuf"
+
 
 cd ~/
 rm -f /root/daftarip
